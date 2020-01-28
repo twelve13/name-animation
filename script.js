c968fa575a1e814109ed0c335b23d879b1d6c972
Caroline Chen <caroline@Carolines-iMac.local>
2020-01-28 16:13:05 -0500

    consolidate similar if statements

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	let bottomSegment = $(".bottom-segment");
 	let fill = $(".fill");
 	let segment13Fill = $("#segment13 .top-fill");
 	let segment15Fill = $("#segment15 .top-fill");
@@ -10,7 +11,7 @@ $(document).ready(function() {
 	let segment3 = $("#segment3");
 
 	let segment5 = $("#segment5");
-	let segment7 = $("#segment7 .fill")
+	let segment7Fill = $("#segment7 .fill")
 
 	
 	let currentPos = 0;
@@ -37,66 +38,39 @@ $(document).ready(function() {
 					segment5.width(100-distance);
 				}
 
-				if(segment7.height() > 120) {
+				if(segment7Fill.height() > 120) {
 					segment13Fill.width(segment13Fill.width()+2);
-				}
-
-				if(segment7.height() > 120) {
+					segment14Fill.width(segment14Fill.width()+4);
 					segment15Fill.width(segment15Fill.width()+2);
 				}
 
-				if(segment7.height() > 60) {
+				if(segment7Fill.height() > 60) {
 					segment12Fill.width(segment12Fill.width()+2);
-				}
-
-				if(segment7.height() > 120) {
-					segment14Fill.width(segment14Fill.width()+4);
 				}
 
 				//scrolling up
 			} else {
-				if(segment1.width() < 100) {
-					segment1.width(segment1.width()+100/distance);
+				if(bottomSegment.width() < 100) {
+					bottomSegment.width(segment1.width()+100/distance);
 				}
 
-				if(segment2.width() < 100) {
-					segment2.width(segment2.width()+100/distance);
-				}
-
-				if(segment3.width() < 100) {
-					segment3.width(segment3.width()+100/distance);
-				}
-
-				if(segment5.width() < 100) {
-					segment5.width(segment5.width()+100/distance);
-				}
-
-				if(segment7.height() < 120) {
+				if(segment7Fill.height() < 120) {
 					segment13Fill.width(segment13Fill.width()-8);
-				}
-
-				if(segment7.height() < 120) {
+					segment14Fill.width(segment14Fill.width()-8);
 					segment15Fill.width(segment15Fill.width()-8);
 				}
 
-				if(segment7.height() < 80) {
+				if(segment7Fill.height() < 80) {
 					segment12Fill.width(segment12Fill.width()-8);
-				}
-
-				if(segment7.height() < 120) {
-					segment14Fill.width(segment14Fill.width()-8);
 				}
 
 				//reset horizontal lines if at top of screen
 				if(distance == 0) {
-					segment1.width(100);
-					segment2.width(100);
-					segment3.width(100);
-					segment5.width(100);
-					segment13Fill.width(0);
-					segment15Fill.width(0);
+					bottomSegment.width(100);
 					segment12Fill.width(0);
+					segment13Fill.width(0);
 					segment14Fill.width(0);
+					segment15Fill.width(0);
 				}
 			}
 	
